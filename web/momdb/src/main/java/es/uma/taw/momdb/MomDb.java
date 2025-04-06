5c0626273cb0dbe83dd2928cc094737b2a732855
RubénO. <rubenoliva@uma.es>
2025-04-06 12:10:58 +0200

    Aun no esta la persitencia de datos

---
+++ web/momdb/src/main/java/es/uma/taw/momdb/MomDb.java
@@ -0,0 +1,13 @@
+package es.uma.taw.momdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomDb {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomDb.class, args);
+    }
+
+}
---
+++ web/momdb/src/test/java/es/uma/taw/momdb/MomDbTests.java
@@ -0,0 +1,13 @@
+package es.uma.taw.momdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomDbTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
